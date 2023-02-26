2f1db671adedf3dd79b93a6420291ae744d3c8f3
zulpikarCydeo <102216429+zulpikarCydeo@users.noreply.github.com>
2023-02-26 19:16:29 +0300

    second push

---
+++ src/test/java/FastTrack4Api/HamcrestMathcers.java
@@ -0,0 +1,11 @@
+package FastTrack4Api;
+
+import org.junit.jupiter.api.*;
+
+public class HamcrestMathcers extends TestBase {
+
+    @Test
+    public void test1(){
+
+    }
+}
--- src/test/java/FastTrack4Api/RequestAndResponse.java
+++ src/test/java/FastTrack4Api/RequestAndResponse.java
@@ -8,36 +8,37 @@ import org.junit.jupiter.api.*;
 
 import java.util.*;
 
+import static io.restassured.RestAssured.config;
 import static io.restassured.RestAssured.given;
 
-public class RequestAndResponse extends TestBase{
+public class RequestAndResponse extends TestBase {
 
     @Test
-    public void test1(){
+    public void test1() {
 
         Response response = given().accept(ContentType.JSON)
                 .when().get("http://3.87.65.105:8000/api/spartans/25");
 
         System.out.println(response.statusCode());
-        Assertions.assertEquals(200,response.statusCode());
+        Assertions.assertEquals(200, response.statusCode());
         Assertions.assertTrue(response.headers().exist());
-        Assertions.assertEquals("application/json",response.header("Content-Type"));
+        Assertions.assertEquals("application/json", response.header("Content-Type"));
 
     }
 
     @Test
-    public void test2(){
+    public void test2() {
 
         Response response = given().accept(ContentType.JSON)
                 .when().get("/api/spartans/25");
 
         Assertions.assertEquals(200, response.statusCode());
-        Assertions.assertEquals("Valentin",response.path("name"));
-        Assertions.assertEquals("Male",response.path("gender"));
+        Assertions.assertEquals("Valentin", response.path("name"));
+        Assertions.assertEquals("Male", response.path("gender"));
     }
 
     @Test
-    public void test3(){
+    public void test3() {
         Response response = given().accept(ContentType.JSON)
                 .when().get("/api/spartans");
 
@@ -51,26 +52,42 @@ public class RequestAndResponse extends TestBase{
     }
 
     @Test
-    public void test4(){
+    public void test4() {
         Response response = given().accept(ContentType.JSON)
-                .and().pathParam("id",20)
+                .and().pathParam("id", 20)
                 .when().get("/api/spartans/{id}");
 
         response.prettyPrint();
     }
 
     @Test
-    public void test5(){
+    public void test5() {
         Response response = given().accept(ContentType.JSON)
-                .and().queryParam("gender","Male")
-                .and().queryParam("nameContains","va")
-                .and().queryParam("nameContains","va")
-                .and().queryParam("nameContains","va")
-                .and().queryParam("nameContains","va")
-                .and().queryParam("nameContains","va")
+                .and().queryParam("gender", "Male")
+                .and().queryParam("nameContains", "va")
                 .when().get("/api/spartans/search");
 
         response.prettyPrint();
+        JsonPath jsonPath = response.jsonPath();
+        Long phone = jsonPath.getLong("content[1].phone");
+        System.out.println(phone);
+
+    }
+
+    @Test
+    public void test6(){
+
+        Map<String, Object> query = new HashMap<>();
+        query.put("nameContains","ha");
+        query.put("nameContains","zu");
+        query.put("nameContains","yu");
+
+        Response response = given().accept(ContentType.JSON)
+                .and().queryParams(query)
+                .when().get("/api/spartans/search");
+
+        response.prettyPrint();
+
     }
 
 
