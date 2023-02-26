e6406bc6d4c1a166fd400b74ac4398c90b8ade27
zulpikarCydeo <102216429+zulpikarCydeo@users.noreply.github.com>
2023-02-26 20:54:20 +0300

    second push

--- src/test/java/FastTrack4Api/Deserilization.java
+++ src/test/java/FastTrack4Api/Deserilization.java
@@ -1,5 +1,6 @@
 package FastTrack4Api;
 
+import FastTrack4Api.POJO.*;
 import io.restassured.http.*;
 import io.restassured.response.*;
 import org.junit.jupiter.api.*;
@@ -33,6 +34,34 @@ public class Deserilization extends TestBase{
         List<Map<String, Object>> maleSpartans = response.as(List.class);
         System.out.println(maleSpartans.get(5));
         System.out.println(maleSpartans);
+    }
+
+
+    // converting response to custom class
+    @Test
+    public void test3(){
+
+        Response response = given().accept(ContentType.JSON)
+                .when().get("/api/spartans/20");
+        Spartan spartan20 = response.as(Spartan.class);
+
+        System.out.println(spartan20);
+    }
+
+    @Test
+    public void test4(){
+
+        Response response = given().accept(ContentType.JSON)
+                .and().queryParam("gender","Male")
+                .and().queryParam("nameContains","va")
+                .when().get("/api/spartans/search");
+
+        Search search = response.as(Search.class);
+        System.out.println(search);
+
 
     }
+
+
 }
+
