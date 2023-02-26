76d18575c38a98a68ad8627148ac0c248a24104f
zulpikarCydeo <102216429+zulpikarCydeo@users.noreply.github.com>
2023-02-26 18:38:10 +0300

    first push

--- src/test/java/FastTrack4Api/RequestAndResponse.java
+++ src/test/java/FastTrack4Api/RequestAndResponse.java
@@ -1,5 +1,8 @@
 package FastTrack4Api;
 
+import io.restassured.*;
+import io.restassured.http.*;
+import io.restassured.response.*;
 import org.junit.jupiter.api.*;
 
 public class RequestAndResponse {
@@ -7,5 +10,15 @@ public class RequestAndResponse {
     @Test
     public void test1(){
 
+        Response response = RestAssured.given().accept(ContentType.JSON)
+                .when().get("http://3.87.65.105:8000/api/spartans/25");
+
+        System.out.println(response.statusCode());
+        Assertions.assertEquals(200,response.statusCode());
+        Assertions.assertTrue(response.headers().exist());
+        Assertions.assertEquals("application/json",response.header("Content-Type"));
+
     }
+
+
 }
